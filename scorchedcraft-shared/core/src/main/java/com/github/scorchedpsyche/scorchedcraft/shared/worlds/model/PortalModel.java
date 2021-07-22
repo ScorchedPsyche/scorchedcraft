643b94335116384d174ed078a16f320cb6b3ed8e
ScorchedPsyche <scorchedpsychegames@gmail.com>
2021-07-22 13:12:32 -0300

    Initial commit

---
+++ scorchedcraft-shared/core/src/main/java/com/github/scorchedpsyche/scorchedcraft/shared/worlds/model/PortalModel.java
@@ -0,0 +1,12 @@
+package com.github.scorchedpsyche.scorchedcraft.shared.worlds.model;
+
+import net.minecraft.world.World;
+
+public class PortalModel {
+    public PortalModel()
+    {
+    }
+
+    private String name;
+    private World world;
+}
